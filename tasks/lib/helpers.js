ccf6868472cb9efc70ee0bf5eac4cb96d719bde3
Arron Schaar <aschaar@gmail.com>
2013-04-17 16:06:40 -0700

    fixed component ordering bug

--- tasks/lib/helpers.js
+++ tasks/lib/helpers.js
@@ -6,17 +6,17 @@ module.exports.findDependencies = function(dict){
 
       var entry = dict[key];
 
-      if (entry.source && entry.source.main) { 
+      if (entry.source && entry.source.main) {
 
         var main = Array.isArray(entry.source.main) ? entry.source.main : [entry.source.main];
         main.forEach(function(item){
 
           var m = item.match(/\.(\w+)$/);
-          
+
           if (m){
-            
+
             var ext = m[1];
-            
+
             if (!files[ext]){
               files[ext] = [];
             }
@@ -24,16 +24,16 @@ module.exports.findDependencies = function(dict){
             var existsAt = files[ext].indexOf(item),
               parentAt = files[ext].indexOf(parentLib);
 
-            if (parentAt > 0 && existsAt === -1){  //insert before
+            if (parentAt >= 0 && existsAt === -1){  //insert before
               files[ext].splice(parentAt,0, item);
             }
-            else if (parentAt > 0 && existsAt > parentAt){  // shuffle higher
+            else if (parentAt >= 0 && existsAt > parentAt){  // shuffle higher
               files[ext].splice(parentAt, 0, item);
               files[ext].splice(existsAt+1,1);
             }
             else if (existsAt === -1){  // doesn't exist yet and no parent
               files[ext].push(item);
-            }            
+            }
           }
           else {
             Console.log("Unknown file type");
