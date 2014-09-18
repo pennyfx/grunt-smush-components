2fccf1567a3fb587a193b1f64e7037558ef1c0d5
Arron <aschaar@gmail.com>
2014-09-17 19:30:23 -0700

    Added packages filter Version bump 1.0.3

--- test/Gruntfile.js
+++ test/Gruntfile.js
@@ -5,12 +5,19 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Configuration to be run (and then tested).
-    'smush-components': {      
+    'smush-components': {
       test: {
         fileMap:{
           js: './dist/components.js',
           css: './dist/components.css'
         }
+      },
+      test2: {
+        fileMap:{
+          js: './dist/components2.js',
+          css: './dist/components2.css'
+        },
+        packages: ['test-package2']
       }
     },
     bumpup: ['bower.json', 'package.json'],
@@ -19,6 +26,6 @@ module.exports = function(grunt) {
   grunt.loadTasks('../tasks');
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('smush', ['smush-components']);
+  grunt.registerTask('test', ['smush-components']);
 
 };
